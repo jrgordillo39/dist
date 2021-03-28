97b5e48dee9b88c2ca18054797000b45e6d93ae6
jrgordillo39 <jjroberto.9@gmail.com>
2021-03-27 20:52:25 -0400

    asd

--- precache-manifest.4cfe81f5747b2bd4e1c65ec7ff9929ea.js
+++ precache-manifest.4cfe81f5747b2bd4e1c65ec7ff9929ea.js
@@ -109,167 +109,167 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
   },
   {
     "revision": "fa5ca5ca0be15d159c72a28b5c0db781",
-    "url": " /img/Avatar.fa5ca5ca.png"
+    "url": "./img/Avatar.fa5ca5ca.png"
   },
   {
     "revision": "e30d0c1b01b7492519a489696c5da1dd",
-    "url": " /img/Background.e30d0c1b.png"
+    "url": "./img/Background.e30d0c1b.png"
   },
   {
     "revision": "95e7ea69cf6e363e37331c466a8c29b8",
-    "url": " /img/Enmascarar_grupo_6.95e7ea69.png"
+    "url": "./img/Enmascarar_grupo_6.95e7ea69.png"
   },
   {
     "revision": "19fc43c1cb72bb24bcc8b78d121880cf",
-    "url": " /img/Grafica_lineas.19fc43c1.png"
+    "url": "./img/Grafica_lineas.19fc43c1.png"
   },
   {
     "revision": "557b8456698f1162df6a626cbdcea527",
-    "url": " /img/Grfica_Winner.557b8456.png"
+    "url": "./img/Grfica_Winner.557b8456.png"
   },
   {
     "revision": "1c15e95302da5d92ac52f013d4f79e58",
-    "url": " /img/Ilustracion_Avion.1c15e953.png"
+    "url": "./img/Ilustracion_Avion.1c15e953.png"
   },
   {
     "revision": "c2d1dedae48ae1a69e097fc6d3617681",
-    "url": " /img/Imagen_Noticia_1.c2d1deda.png"
+    "url": "./img/Imagen_Noticia_1.c2d1deda.png"
   },
   {
     "revision": "a667cad2a1c8c4bd8dceeca83538b9f0",
-    "url": " /img/Imagen_Noticia_1@2x.a667cad2.png"
+    "url": "./img/Imagen_Noticia_1@2x.a667cad2.png"
   },
   {
     "revision": "85696bd8dddfec28805df294b10e228d",
-    "url": " /img/Imagen_Noticia_2.85696bd8.png"
+    "url": "./img/Imagen_Noticia_2.85696bd8.png"
   },
   {
     "revision": "24d2a76912138279b7b4d5a4917e6f39",
-    "url": " /img/Imagen_Noticia_2@2x.24d2a769.png"
+    "url": "./img/Imagen_Noticia_2@2x.24d2a769.png"
   },
   {
     "revision": "2d8f635d1b10c5f8703ed6089e26529c",
-    "url": " /img/Imagen_Noticia_3.2d8f635d.png"
+    "url": "./img/Imagen_Noticia_3.2d8f635d.png"
   },
   {
     "revision": "3cfd46cd82e84790586e9adb031ca3ec",
-    "url": " /img/Imagen_Noticia_3@2x.3cfd46cd.png"
+    "url": "./img/Imagen_Noticia_3@2x.3cfd46cd.png"
   },
   {
     "revision": "277d525ca7b27cab454b0171e4cf0fc4",
-    "url": " /img/Imagen_Noticia_4.277d525c.png"
+    "url": "./img/Imagen_Noticia_4.277d525c.png"
   },
   {
     "revision": "eaa29bd96ba77b745b3149443544032a",
-    "url": " /img/Imagen_Noticia_4@2x.eaa29bd9.png"
+    "url": "./img/Imagen_Noticia_4@2x.eaa29bd9.png"
   },
   {
     "revision": "0f7b7a28b99ece4feb393027362d07f9",
-    "url": " /img/Llamadas_ik.0f7b7a28.png"
+    "url": "./img/Llamadas_ik.0f7b7a28.png"
   },
   {
     "revision": "a6df00f361f87a8960f956b2a2d2e1d3",
-    "url": " /img/Ofertas-Background-3.a6df00f3.png"
+    "url": "./img/Ofertas-Background-3.a6df00f3.png"
   },
   {
     "revision": "5c69ffc061bc9632776db05f59668046",
-    "url": " /img/Ofertas-Background.5c69ffc0.png"
+    "url": "./img/Ofertas-Background.5c69ffc0.png"
   },
   {
     "revision": "8a50b533995a48ea25314f4da577bdf8",
-    "url": " /img/Paquetes.8a50b533.png"
+    "url": "./img/Paquetes.8a50b533.png"
   },
   {
     "revision": "644f281bc1f953c1c8fe826ec6c583d0",
-    "url": " /img/Pasaportes.644f281b.png"
+    "url": "./img/Pasaportes.644f281b.png"
   },
   {
     "revision": "3d9c815026588341d4bc761db05d0a2b",
-    "url": " /img/Promo App.3d9c8150.png"
+    "url": "./img/Promo App.3d9c8150.png"
   },
   {
     "revision": "ac5c131e59cb7c75a6ec175cfd538113",
-    "url": " /img/Recargas_if@2x.ac5c131e.png"
+    "url": "./img/Recargas_if@2x.ac5c131e.png"
   },
   {
     "revision": "462b6d61d3a69b2b0b427fb29829b89f",
-    "url": " /img/Remesas_i.462b6d61.png"
+    "url": "./img/Remesas_i.462b6d61.png"
   },
   {
     "revision": "7a21789bc7ea5e320ff8d339a06b1d14",
-    "url": " /img/combo_mixto.7a21789b.png"
+    "url": "./img/combo_mixto.7a21789b.png"
   },
   {
     "revision": "7a21789bc7ea5e320ff8d339a06b1d14",
-    "url": " /img/combo_mixto.png"
+    "url": "./img/combo_mixto.png"
   },
   {
     "revision": "0177b94d71dc1fd0f58fb848ecb0f966",
-    "url": " /img/conos_identificativos_ip.0177b94d.png"
+    "url": "./img/conos_identificativos_ip.0177b94d.png"
   },
   {
     "revision": "e2c3d3b051617ff3f75e0acc1f7528bf",
-    "url": " /img/cooking.png"
+    "url": "./img/cooking.png"
   },
   {
     "revision": "0c48cd26e981daa6c28be2304d5ccd62",
-    "url": " /img/cubamax.0c48cd26.jpeg"
+    "url": "./img/cubamax.0c48cd26.jpeg"
   },
   {
     "revision": "0c48cd26e981daa6c28be2304d5ccd62",
-    "url": " /img/cubamax.jpeg"
+    "url": "./img/cubamax.jpeg"
   },
   {
     "revision": "96f81a8254d1b587e121e28a94e73e0a",
-    "url": " /img/cubamax_logo@2x.96f81a82.png"
+    "url": "./img/cubamax_logo@2x.96f81a82.png"
   },
   {
     "revision": "1b6c63ee856f08a9f11c9a8adcf468d6",
-    "url": " /img/cubamax_logo_ne@2x.1b6c63ee.png"
+    "url": "./img/cubamax_logo_ne@2x.1b6c63ee.png"
   },
   {
     "revision": "a086441df607e8d6587a3ef0c9a9ea0b",
-    "url": " /img/download_app.png"
+    "url": "./img/download_app.png"
   },
   {
     "revision": "5d10d00980077f67990f73b1961bc46e",
-    "url": " /img/fa-brands-400.5d10d009.svg"
+    "url": "./img/fa-brands-400.5d10d009.svg"
   },
   {
     "revision": "d0fdc7c20b0b0b1aaf27327547735fa3",
-    "url": " /img/fa-regular-400.d0fdc7c2.svg"
+    "url": "./img/fa-regular-400.d0fdc7c2.svg"
   },
   {
     "revision": "2306f642b31b774ba1d8b6b784539e19",
-    "url": " /img/fa-solid-900.2306f642.svg"
+    "url": "./img/fa-solid-900.2306f642.svg"
   },
   {
     "revision": "912ec66d7572ff821749319396470bde",
-    "url": " /img/fontawesome-webfont.912ec66d.svg"
+    "url": "./img/fontawesome-webfont.912ec66d.svg"
   },
   {
     "revision": "5b683905d52c426492c0409d3c362be0",
-    "url": " /img/footer.5b683905.png"
+    "url": "./img/footer.5b683905.png"
   },
   {
     "revision": "71c806428f9d8c76f8dd491ee177382c",
-    "url": " /img/iconfinder_map-marker_299087.71c80642.png"
+    "url": "./img/iconfinder_map-marker_299087.71c80642.png"
   },
   {
     "revision": "e30d0c1b01b7492519a489696c5da1dd",
-    "url": " /img/inicio.e30d0c1b.png"
+    "url": "./img/inicio.e30d0c1b.png"
   },
   {
     "revision": "82b9c7a5a3f405032b1db71a25f67021",
-    "url": " /img/logo.82b9c7a5.png"
+    "url": "./img/logo.82b9c7a5.png"
   },
   {
     "revision": "151b615ad33012eff3396ab0c5809ce9",
-    "url": " /img/sunshine.jpg"
+    "url": "./img/sunshine.jpg"
   },
   {
     "revision": "149b80a91fb241f659544daafcd0ad4b",
-    "url": " /img/tienda-banner.149b80a9.png"
+    "url": "./img/tienda-banner.149b80a9.png"
   },
   {
     "revision": "a7900d07b665bceb9aa52821e49d0721",
