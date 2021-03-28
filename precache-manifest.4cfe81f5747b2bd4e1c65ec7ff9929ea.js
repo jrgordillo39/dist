8af5d04e5df1c4c3031760a5fe0611feb807f15b
jrgordillo39 <jjroberto.9@gmail.com>
2021-03-27 22:02:21 -0400

    asd

--- precache-manifest.4cfe81f5747b2bd4e1c65ec7ff9929ea.js
+++ precache-manifest.4cfe81f5747b2bd4e1c65ec7ff9929ea.js
@@ -1,111 +1,111 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
     "revision": "3ea954ebee924d86da5c",
-    "url": " /css/app.2877f14c.css"
+    "url": "./css/app.2877f14c.css"
   },
   {
     "revision": "22361e9d5a0673b8b1e8",
-    "url": " /css/chunk-vendors.1719d63d.css"
+    "url": "./css/chunk-vendors.1719d63d.css"
   },
   {
     "revision": "2a61b00fcc9b1afbf99be145777b1b72",
-    "url": " /fonts/MaterialIcons-Regular.2a61b00f.eot"
+    "url": "./fonts/MaterialIcons-Regular.2a61b00f.eot"
   },
   {
     "revision": "51cf1d641ae503ece727e7d84b182524",
-    "url": " /fonts/MaterialIcons-Regular.51cf1d64.ttf"
+    "url": "./fonts/MaterialIcons-Regular.51cf1d64.ttf"
   },
   {
     "revision": "703cf8f274fbb265d49c6262825780e1",
-    "url": " /fonts/MaterialIcons-Regular.703cf8f2.woff2"
+    "url": "./fonts/MaterialIcons-Regular.703cf8f2.woff2"
   },
   {
     "revision": "84a37de85c17f186652a179b1145392f",
-    "url": " /fonts/MaterialIcons-Regular.84a37de8.woff"
+    "url": "./fonts/MaterialIcons-Regular.84a37de8.woff"
   },
   {
     "revision": "580d0778ad254335be45bf58bb449f43",
-    "url": " /fonts/Raleway-Regular.580d0778.ttf"
+    "url": "./fonts/Raleway-Regular.580d0778.ttf"
   },
   {
     "revision": "0c9f225e8f69c622f681cf1ed973cc3d",
-    "url": " /fonts/fa-brands-400.0c9f225e.woff2"
+    "url": "./fonts/fa-brands-400.0c9f225e.woff2"
   },
   {
     "revision": "0ce1e868452204695c8ac1c70f693c2d",
-    "url": " /fonts/fa-brands-400.0ce1e868.woff"
+    "url": "./fonts/fa-brands-400.0ce1e868.woff"
   },
   {
     "revision": "d28c96cabe5302f1b2c90275cd3f4e69",
-    "url": " /fonts/fa-brands-400.d28c96ca.eot"
+    "url": "./fonts/fa-brands-400.d28c96ca.eot"
   },
   {
     "revision": "dfe5aa4344a2d8a29aec8d83fb3fb14e",
-    "url": " /fonts/fa-brands-400.dfe5aa43.ttf"
+    "url": "./fonts/fa-brands-400.dfe5aa43.ttf"
   },
   {
     "revision": "0232fe8b09de812c88e6cc27f57a3a88",
-    "url": " /fonts/fa-regular-400.0232fe8b.eot"
+    "url": "./fonts/fa-regular-400.0232fe8b.eot"
   },
   {
     "revision": "398be22b2308585b84da8e858e2d7b60",
-    "url": " /fonts/fa-regular-400.398be22b.ttf"
+    "url": "./fonts/fa-regular-400.398be22b.ttf"
   },
   {
     "revision": "3cace4a04d941b5981ba32c6ce9afae1",
-    "url": " /fonts/fa-regular-400.3cace4a0.woff"
+    "url": "./fonts/fa-regular-400.3cace4a0.woff"
   },
   {
     "revision": "847712aaabbeba674afdda86d31cab17",
-    "url": " /fonts/fa-regular-400.847712aa.woff2"
+    "url": "./fonts/fa-regular-400.847712aa.woff2"
   },
   {
     "revision": "00bb62bc33034479d5e82a0fd1c5efe3",
-    "url": " /fonts/fa-solid-900.00bb62bc.eot"
+    "url": "./fonts/fa-solid-900.00bb62bc.eot"
   },
   {
     "revision": "260d033f79f75d670fd09d8cf7c8bf8d",
-    "url": " /fonts/fa-solid-900.260d033f.ttf"
+    "url": "./fonts/fa-solid-900.260d033f.ttf"
   },
   {
     "revision": "4bfbf7eb4b19d9ff9293eb177b6d0070",
-    "url": " /fonts/fa-solid-900.4bfbf7eb.woff"
+    "url": "./fonts/fa-solid-900.4bfbf7eb.woff"
   },
   {
     "revision": "9ae050d1876ac1763eb6afe4264e6d5a",
-    "url": " /fonts/fa-solid-900.9ae050d1.woff2"
+    "url": "./fonts/fa-solid-900.9ae050d1.woff2"
   },
   {
     "revision": "674f50d287a8c48dc19ba404d20fe713",
-    "url": " /fonts/fontawesome-webfont.674f50d2.eot"
+    "url": "./fonts/fontawesome-webfont.674f50d2.eot"
   },
   {
     "revision": "af7ae505a9eed503f8b8e6982036873e",
-    "url": " /fonts/fontawesome-webfont.af7ae505.woff2"
+    "url": "./fonts/fontawesome-webfont.af7ae505.woff2"
   },
   {
     "revision": "b06871f281fee6b241d60582ae9369b9",
-    "url": " /fonts/fontawesome-webfont.b06871f2.ttf"
+    "url": "./fonts/fontawesome-webfont.b06871f2.ttf"
   },
   {
     "revision": "fee66e712a8a08eef5805a46892932ad",
-    "url": " /fonts/fontawesome-webfont.fee66e71.woff"
+    "url": "./fonts/fontawesome-webfont.fee66e71.woff"
   },
   {
     "revision": "147e3378b44bc9570418b1eece10dd7c",
-    "url": " /fonts/materialdesignicons-webfont.147e3378.woff"
+    "url": "./fonts/materialdesignicons-webfont.147e3378.woff"
   },
   {
     "revision": "174c02fc4609e8fc4389f5d21f16a296",
-    "url": " /fonts/materialdesignicons-webfont.174c02fc.ttf"
+    "url": "./fonts/materialdesignicons-webfont.174c02fc.ttf"
   },
   {
     "revision": "64d4cf64afd77a4ad2713f648eb920e6",
-    "url": " /fonts/materialdesignicons-webfont.64d4cf64.eot"
+    "url": "./fonts/materialdesignicons-webfont.64d4cf64.eot"
   },
   {
     "revision": "7a44ea195f395e1d086010e44555a5c4",
-    "url": " /fonts/materialdesignicons-webfont.7a44ea19.woff2"
+    "url": "./fonts/materialdesignicons-webfont.7a44ea19.woff2"
   },
   {
     "revision": "fa5ca5ca0be15d159c72a28b5c0db781",
@@ -273,22 +273,22 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
   },
   {
     "revision": "a7900d07b665bceb9aa52821e49d0721",
-    "url": " /index.html"
+    "url": "./index.html"
   },
   {
     "revision": "3ea954ebee924d86da5c",
-    "url": " /js/app.fd284307.js"
+    "url": "./js/app.fd284307.js"
   },
   {
     "revision": "22361e9d5a0673b8b1e8",
-    "url": " /js/chunk-vendors.3578483a.js"
+    "url": "./js/chunk-vendors.3578483a.js"
   },
   {
     "revision": "b7e965513f9ba4e0842efdfa5b81dd33",
-    "url": " /manifest.json"
+    "url": "./manifest.json"
   },
   {
     "revision": "735ab4f94fbcd57074377afca324c813",
-    "url": " /robots.txt"
+    "url": "./robots.txt"
   }
 ]);
